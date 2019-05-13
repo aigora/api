7c6fb7a164ed2db5e6b47842e25ba2aa89077fd7
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2019-05-13 12:25:43 +0200

    Corrige código de grupo en xls

--- xls.R
+++ xls.R
@@ -2,7 +2,7 @@ library(openxlsx)
 
 teamsByRepo <- fread("csv/teamsByRepo.csv")
 twE105 <- fread("csv/twE105.csv")
-twE100 <- fread("csv/twE105.csv")
+twE100 <- fread("csv/twE100.csv")
 twA104 <- fread("csv/twA104.csv")
 twA109 <- fread("csv/twA109.csv")
 twQ103 <- fread("csv/twQ103.csv")
