2da4d3f283f9f59ade953e5dbdaf083ed65951d0
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2021-05-23 18:57:50 +0200

    Incorpora recopilación de estadísticas de *cada* commit al código

--- commits.R
+++ commits.R
@@ -27,50 +27,74 @@ names(twCommits) <- twRepos
 
 ## -------------------------------------------------
 ## Ejecutar únicamente para solicitar datos a GitHub
-twCommits <- lapply(twRepos, function(x)
+twCommits <- lapply(twRepos, function(repo)
 {
-    cat(x, "\n")
+    cat(repo, "\n")
     parsed <- getPages(paste0("/repos/aigora/",
-                         x,
+                         repo,
                          "/commits"))
     
     author <- lapply(parsed, function(x)
         x$commit$author[c("name", "date")])
-    res <- do.call(rbind, author)
+    
+    author <- rbindlist(author)
+
+    sha <- lapply(parsed, function(x) x$sha)
+
+    sha <- do.call(c, sha)
+
+    commits <-  lapply(sha, function(sha)
+        getPages(paste0("/repos/aigora/",
+                         repo,
+                        "/commits/",
+                        sha)))
+    stats <- lapply(commits, function(x)
+        x$stats)
+    stats <- rbindlist(stats)
+
+    res <- cbind(author, stats, sha)
+    
     write.csv(res,
-               file = paste0("csv/commits_", x, ".csv"),
+               file = paste0("csv/commits_", repo, ".csv"),
                row.names = FALSE)
-    res
+
 })
 names(twCommits) <- twRepos
 
 
-exCommits <- lapply(exRepos, function(x)
-{
-    cat(x, "\n")
-    parsed <- getPages(paste0("/repos/aigora/",
-                         x,
-                         "/commits"))
+## exCommits <- lapply(exRepos, function(x)
+## {
+##     cat(x, "\n")
+##     parsed <- getPages(paste0("/repos/aigora/",
+##                          x,
+##                          "/commits"))
     
-    author <- lapply(parsed, function(x)
-        x$commit$author[c("name", "date")])
-    res <- do.call(rbind, author)
-    write.csv(res,
-               file = paste0("csv/commits_", x, ".csv"),
-               row.names = FALSE)
-    res
-})
-names(exCommits) <- exRepos
+##     author <- lapply(parsed, function(x)
+##         x$commit$author[c("name", "date")])
+##     res <- do.call(rbind, author)
+##     write.csv(res,
+##                file = paste0("csv/commits_", x, ".csv"),
+##                row.names = FALSE)
+##     res
+## })
+## names(exCommits) <- exRepos
 
 ##------------------------------------------------------
 
 
 resumen <-  lapply(seq_along(twCommits), function(i)
 {
-    data.frame(repo = twRepos[i],
-               grupo = substr(twRepos[i], 3, 6),
-               N = nrow(twCommits[[i]]),
-               last = as.Date(twCommits[[i]][1, 2][[1]]))
+    repo <- twRepos[i]
+    info <- twCommits[[i]]
+    N <- nrow(info)
+    xxx <- data.frame(repo = repo,
+               grupo = substr(repo, 3, 6),
+               N = N - 1, ## El primer commit es el bot de GH Classroom
+               total = sum(info$total[-N]), ## No tengo en cuenta el primer commit para las estadísticas
+               add = sum(info$add[-N]),
+               del = sum(info$del[-N]),
+               last = as.Date(info$date[1])## Commit más reciente
+               )
 })
 resumen <- do.call(rbind, resumen)
 rownames(resumen) <- NULL
