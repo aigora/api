5c3b54e1eb3ab1e89638248ddf651d33d4289ccd
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2022-03-28 21:25:48 +0200

    Ejecuta issues y milestones en E105

--- issues.R
+++ issues.R
@@ -3,17 +3,24 @@ source("API.R")
 issues <- read.csv2("issues/issues.txt")
 milestones <- read.csv2("issues/milestones.txt")
 
+teams <- fread('csv/teams.csv')
+reposE105 <- teams[group == "E105", repo]
 
-for (i in seq_len(nrow(milestones)))
-    ghMilestone(repo,
-                title = milestones[i, "Title"],
-                description = milestones[i, "Description"],
-                date = milestones[i, "Date"])
+lapply(reposE105, function(repo)
+{
+    
+    for (i in seq_len(nrow(milestones)))
+        ghMilestone(repo,
+                    title = milestones[i, "Title"],
+                    description = milestones[i, "Description"],
+                    date = milestones[i, "Date"])
 
-for (i in seq_len(nrow(issues)))
-    ghIssue(repo,
-            title = issues[i, "Title"],
-            body = issues[i, "Body"],
-            milestone = issues[i, "Milestone"]
-            )
+    for (i in seq_len(nrow(issues)))
+        ghIssue(repo,
+                title = issues[i, "Title"],
+                body = issues[i, "Body"],
+                milestone = issues[i, "Milestone"]
+                )
+
+})
 
